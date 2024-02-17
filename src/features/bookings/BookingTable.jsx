1347e375f726774e67ae7ec81bc8bda7174e5531
TonySN90 <tony.heider90@gmail.com>
2024-02-17 08:44:30 +0100

    add sort functionality

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -5,6 +5,8 @@ import Menus from "../../ui/Menus";
 function BookingTable() {
   const bookings = [];
 
+  // if()
+
   return (
     <Menus>
       <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
