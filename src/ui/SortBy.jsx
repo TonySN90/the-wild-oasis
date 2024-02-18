4aaaec1327b3735f442992d410f3b321f7caed05
TonySN90 <tony.heider90@gmail.com>
2024-02-18 13:02:18 +0100

    add Bookings-Pagination

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -4,7 +4,6 @@ import Select from "./Select";
 function SortBy({ options }) {
   const [searchParams, setSearchParams] = useSearchParams();
   const sortBy = searchParams.get("sortBy") || "";
-  console.log(sortBy);
 
   function handleChange(e) {
     searchParams.set("sortBy", e.target.value);
