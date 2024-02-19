5dec1b7296830350abd38da1a8311b5e0e5601f9
TonySN90 <tony.heider90@gmail.com>
2024-02-19 11:04:22 +0100

    add Auth | logout + user signup

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,20 @@
 import supabase from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        fullName,
+        avatar: "",
+      },
+    },
+  });
+  if (error) throw new Error(error.message);
+  return data;
+}
+
 export async function login({ email, password }) {
   let { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -17,9 +32,11 @@ export async function getCurrentUser() {
 
   const { data, error } = await supabase.auth.getUser();
 
-  console.log(data);
-
   if (error) throw new Error(error.message);
-
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
